adc9cd7c9c8360591b4d7831c91ac896f0a7561c
autohero2022 <113869188+autohero2022@users.noreply.github.com>
2022-10-01 12:16:07 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/example/autoheroback/securite/config/WebSecurityConfig.java
+++ src/main/java/com/example/autoheroback/securite/config/WebSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebSecurityConfig
     CorsConfigurationSource corsConfigurationSource()
     {
         CorsConfiguration configuration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://31.207.33.183:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
