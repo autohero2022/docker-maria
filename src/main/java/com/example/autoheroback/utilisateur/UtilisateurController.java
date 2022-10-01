18c809c23b6913d86c22545b2012e6df71accd63
autohero2022 <113869188+autohero2022@users.noreply.github.com>
2022-10-01 21:16:47 +0100

    Update UtilisateurController.java

--- src/main/java/com/example/autoheroback/utilisateur/UtilisateurController.java
+++ src/main/java/com/example/autoheroback/utilisateur/UtilisateurController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping(path ="api/v1/utilisateur")
 @CrossOrigin(origins = "*")
 @AllArgsConstructor
