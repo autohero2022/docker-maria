b8d963c0572becafa4b3077fb351954a6c1a0d40
autohero2022 <113869188+autohero2022@users.noreply.github.com>
2022-10-01 17:26:10 +0100

    Update UtilisateurService.java

--- src/main/java/com/example/autoheroback/utilisateur/UtilisateurService.java
+++ src/main/java/com/example/autoheroback/utilisateur/UtilisateurService.java
@@ -109,7 +109,7 @@ public class UtilisateurService implements UserDetailsService {
                         request.getPassword()
                 ));
 
-        String link = "http://31.207.33.183:8080/api/v1/utilisateur/confirm?token=" + token;
+        String link = "https://31.207.33.183:80/api/v1/utilisateur/confirm?token=" + token;
 
         emailSender.send(
                 request.getEmail(),
